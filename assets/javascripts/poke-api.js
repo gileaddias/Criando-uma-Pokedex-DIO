281ead80bd382c0ca95d15ad7fddb6ddf8e32c5d
gileaddias <gfarias.gpf@gmail.com>
2024-11-27 13:31:12 -0300

    criando mecanismo de paginação para limitar a quantidade de itens por página

--- assets/javascripts/poke-api.js
+++ assets/javascripts/poke-api.js
@@ -1,11 +1,11 @@
 
 const pokeApi = {}
 
- function convertPokeApiDetailPokemon(pokeDetail) {
+function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
-    
+
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
@@ -17,19 +17,19 @@ const pokeApi = {}
     return pokemon
 }
 
-pokeApi.getPokemonsDetail = (pokemon) => {
+pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-        .then((response)  => response.json())
-        .then(convertPokeApiDetailPokemon)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 1, limit = 5) =>  {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
-        .then((detailsRequests) => Promise.all(detailsRequests))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
-}     
-
+}
